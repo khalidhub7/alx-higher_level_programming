0e91a537d115bb1a1d5050dc68fc0040e45cfc2c
khalidhub7 <louguikhalid7@gmail.com>
2023-11-29 14:18:19 -0800

    up

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,13 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -22,3 +23,4 @@ void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
+
