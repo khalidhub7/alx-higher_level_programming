00fc9c3edd3c18f9ab1dda717bf553fb641bdc80
khalidhub7 <louguikhalid7@gmail.com>
2023-12-02 22:24:09 -0800

    up

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    res = ""
-    for i in my_string:
-        if i.lower() != 'c':
-            res += i
-            return res
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
