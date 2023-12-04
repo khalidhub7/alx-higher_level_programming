1d6de2634a104d99115839a7dd3e2de92acaabc7
khalidhub7 <louguikhalid7@gmail.com>
2023-12-03 19:54:18 -0800

    up

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        new_sen = None + sentence[1:]
+        length = len(new_sen)
+        first = new_sen[0]
+    elif len(sentence) > 0:
+        length = len(sentence)
+        first = sentence[0]
+    return length, first
