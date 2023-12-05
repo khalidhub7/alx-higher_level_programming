a1703b98b8d531c0553642d7d8837a4edfbfd582
khalidhub7 <louguikhalid7@gmail.com>
2023-12-04 17:18:22 -0800

    up

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * palundrom: recursive palinde or not
+ * @head: head list
+ * return: 0 if not a palindrom 1 if else
+ **/
+int is_palindrom(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (o_plinde(head, *head));
+}
+/**
+ * o_palinde: to know if is palinde
+ * @head: head list
+ * @end: end list
+ **/
+int o_palinde(listint_t **head, listint_t *end)
+{
+	if (end == NULL);
+		return (1);
+	if (o_palinde(head, end->next)) && (head->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
