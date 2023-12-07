44bf6afd3fab8e3a7e3e91b292fa538739140488
khalidhub7 <louguikhalid7@gmail.com>
2023-12-07 01:26:39 -0800

    up

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,5 @@ def update_dictionary(a_dictionary, key, value):
     for i in a_dictionary:
         if i == key:
             a_dictionary[key] = value
-    a_dictionary['cc'] = value
+    a_dictionary[key] = value
     return(a_dictionary)
