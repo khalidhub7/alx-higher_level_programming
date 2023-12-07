478584b05c468f9a34ad44f7b2346a236a093885
khalidhub7 <louguikhalid7@gmail.com>
2023-12-06 21:28:49 -0800

    up

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    my = []
+    for i in my_list:
+        if i == search:
+            i = replace
+            my.append(i)
+        elif i != search:
+            my.append(i)
+            return my
