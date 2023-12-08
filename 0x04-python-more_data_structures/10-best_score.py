7906272ccd19951e53a89a02926704e511fee214
khalidhub7 <louguikhalid7@gmail.com>
2023-12-07 20:15:48 -0800

    up

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        gg = max(a_dictionary, key=lambda x: a_dictionary[x])
+        return gg
