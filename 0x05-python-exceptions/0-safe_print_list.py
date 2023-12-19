d301a30f4e2c9b02f93c0ec61e65ea83c9c78ce7
khalidhub7 <louguikhalid7@gmail.com>
2023-12-19 04:38:44 -0800

    up

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,5 +11,5 @@ def safe_print_list(my_list=[], x=0):
             for i in my_list:
                 len += 1
                 print(i, end="")
-            print()
-            return len
+    print()
+    return len
