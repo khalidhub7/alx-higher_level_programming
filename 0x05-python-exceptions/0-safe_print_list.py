f000353a455104467bd00698618334000328606d
khalidhub7 <louguikhalid7@gmail.com>
2023-12-19 03:57:37 -0800

    up

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
                 print(i, end="")
         print()
         return x
-    except IndexError:
+        except IndexError:
             len = 0
             for i in my_list:
                 len += 1
