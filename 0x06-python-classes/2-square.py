8781e3ae74c20f6f0d89fdb1b135c0d2b92609c7
khalidhub7 <louguikhalid7@gmail.com>
2023-12-26 22:24:37 -0800

    up

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,10 +2,10 @@
 """my class in python."""
 
 
-
 class Square:
     """def square hhh """
     def __init__(self, size = 0):
+        """drna 2 raises type error, value error"""
         if type(size) == str:
             raise TypeError('size must be an integer')
         elif type(size) != str:
