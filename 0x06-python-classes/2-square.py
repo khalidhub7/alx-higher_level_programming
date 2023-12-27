7ef48a97669ca2d018806b92abc8ee2a8451f8d1
khalidhub7 <louguikhalid7@gmail.com>
2023-12-26 22:05:34 -0800

    up

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,9 @@
 class Square:
     """def square hhh """
     def __init__(self, size = 0):
-        self.__size = size
-    @property
-    def size(self, size):
-        if size == int:
-            return self.__size
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        raise TypeError("size must be an integer")
+        if type(size) == str:
+            raise TypeError('size must be an integer')
+        elif type(size) != str:
+            self.__size = size
+            if size < 0:
+                raise ValueError('size must be >= 0')
