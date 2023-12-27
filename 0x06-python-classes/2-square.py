d6dca3ef79610180f1dfcad38455a62c7202f179
khalidhub7 <louguikhalid7@gmail.com>
2023-12-26 22:49:47 -0800

    up

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,7 +13,6 @@ class Square:
         """
         if type(size) == str:
             raise TypeError('size must be an integer')
-        elif type(size) != str:
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = size
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
