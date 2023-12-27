ffc4514d1a5b12593e161b8ade7841a9515b5152
khalidhub7 <louguikhalid7@gmail.com>
2023-12-27 00:19:15 -0800

    up

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,6 +4,7 @@
 
 class Square:
     """define square"""
+
     def __init__(self, size=0):
         """define raises"""
         if not isinstance(size, int):
@@ -11,6 +12,7 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
         """return squared of size"""
         return self.__size ** 2
