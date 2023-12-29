57c6304a416ae4ca344cbe62e6fff7b46759458a
khalidhub7 <louguikhalid7@gmail.com>
2023-12-28 16:42:45 -0800

    up

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" define square"""
+"""define square"""
 
 
 class Square:
-    """define """
+    """inialization """
     def __init__(self, size=0):
         self.size = size
+
     @property
-    """ getter && sitter && raise errors"""
     def size(self):
+        """ get w set w raises"""
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -17,12 +19,14 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """define squared"""
         return self.__size ** 2
+
     def my_print(self):
         """define print function"""
         for i in range(self.size):
-            print("#" * self.size)
-        if self.size == 0:
-            print()
+            for j in range(self.size):
+                print("#", end="\n" if j is self.size - 1 and i != j else "")
+        print()
