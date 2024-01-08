448486e1bbd4a51fdff7f149d2315af6b6cf9f79
khalidhub7 <louguikhalid7@gmail.com>
2024-01-08 04:40:39 -0800

    up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ module lockup methode """
 
+
+
 def lookup(obj):
-    """  function returns the list of available attributes and methods of an object """
+    """ function returns the list of available attributes and methods of an object """
     return dir(obj)
