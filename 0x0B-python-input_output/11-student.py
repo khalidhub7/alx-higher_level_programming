e2e72eaa7c4a13800f76c391938d4d5c7ea8ec5b
khalidhub7 <louguikhalid7@gmail.com>
2024-01-11 02:35:56 -0800

    up

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+'''my module'''
+
+
+import json
 
 
 class Student:
-    """Represent a student."""
-
+    '''class'''
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        '''initia'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        '''return dict of student'''
+        try:
+            for i in attrs:
+                if type(i) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        m = dict()
+        for k, v in self.__dict__.items():
+            if k in attrs:
+                m[k] = v
+        return m
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student.
-
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+        '''replace dict'''
+        a = json.loads(json)
+        for i, j in a.items():
+            if i in self.__dict__:
+                self.__doc__[i] = j
