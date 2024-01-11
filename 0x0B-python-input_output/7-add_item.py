07bc679ebb641d5439229f18a2c3793c6032f057
khalidhub7 <louguikhalid7@gmail.com>
2024-01-10 18:51:09 -0800

    up

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,6 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
-
 a = list(sys.argv[1:])
 
 try:
@@ -15,4 +14,4 @@ except Exception:
     b = []
 
 b += a
-save_to_json_file(a, "add_item.json")
+sh: 1: q: not found
