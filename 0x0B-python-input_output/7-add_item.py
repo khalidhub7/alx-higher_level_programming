2a7f9bd0d4dd408d0cf56654ae3b0ae335bc1922
khalidhub7 <louguikhalid7@gmail.com>
2024-01-10 18:49:51 -0800

    up

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,14 +7,12 @@ save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
 
-a = []
-for i in (sys.argv)[1:]:
-    a.append(i)
+a = list(sys.argv[1:])
 
 try:
     b = load_from_json_file("add_item.json")
 except Exception:
     b = []
 
-b.extend(a)
+b += a
 save_to_json_file(a, "add_item.json")
