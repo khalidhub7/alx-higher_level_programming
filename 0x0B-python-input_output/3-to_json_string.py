744364117dddde4d54d223d2a912b5fe680cfee5
khalidhub7 <louguikhalid7@gmail.com>
2024-01-10 17:36:23 -0800

    up

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,9 +3,6 @@
 
 
 import json
-
-
 def to_json_string(my_obj):
     '''return json string'''
-    with open(my_obj, "r", encoding='UTF8') as a:
-        return json.dumps(a)
+    return json.dumps(my_obj)
