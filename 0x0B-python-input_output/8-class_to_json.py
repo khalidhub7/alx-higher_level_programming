5f576bee747c82881ba1a13f0168a8f7202871bb
khalidhub7 <louguikhalid7@gmail.com>
2024-01-10 23:23:59 -0800

    up

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Defines a Python class-to-JSON function."""
+'''my module'''
+
+
+import sys
+import json
 
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+    json.loads(obj)
