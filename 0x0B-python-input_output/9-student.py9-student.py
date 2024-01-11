af98f52d8670cd0358436319515080169afaeebd
khalidhub7 <louguikhalid7@gmail.com>
2024-01-10 23:42:39 -0800

    up

---
+++ 0x0B-python-input_output/9-student.py9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''my module'''
+
+
+class Student:
+    '''class'''
+    def __init__(self, first_name, last_name, age):
+        '''initia'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        '''return dict of student'''
+        return self.__dict__
