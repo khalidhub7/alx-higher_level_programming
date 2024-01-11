a0f603395b6a6f919ba13402231c02ba370fac8a
khalidhub7 <louguikhalid7@gmail.com>
2024-01-11 04:30:59 -0800

    up

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,12 +4,14 @@
 def append_after(filename="", search_string="", new_string=""):
     '''append str for specifique search str'''
     with open(filename, "r", encoding='UTF8') as a:
-        b = a.readlines()
         c = []
-        c.append(b)
-        for i in c:
-            if i == search_string:
-                i += new_string
+        while True:
+            b = a.readline()
+            c.append(b)
+            for i in c:
+                if i == search_string:
+                    i += search_string
+                i = i
     with open(filename, 'w', encoding='UTF8') as x:
-        z = x.write(c)
+        z = x.writelines(i)
         return z
