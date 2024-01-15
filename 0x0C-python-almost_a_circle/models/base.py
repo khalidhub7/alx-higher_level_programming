9b5b23f651e3b0da7a4e262e796b7be59989a8df
khalidhub7 <louguikhalid7@gmail.com>
2024-01-15 09:35:06 -0800

    up

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ class Base:
     __nb_objects = 0
     def __init__(self, id=None):
         '''constructor'''
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
