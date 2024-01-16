acd23113e2beb35dd24fe59212dfe252cc50d6fa
khalidhub7 <louguikhalid7@gmail.com>
2024-01-16 00:26:58 -0800

    up

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,7 @@ class Rectangle(Base):
         2-- update by taking care of x and y
         '''
         str_updated = '\n' * self.y + \
-                (' ' * self.x + '#' * self.width + '\n') * self.height
+                      (' ' * self.x + '#' * self.width + '\n') * self.height
         print(str_updated, end='')
 
     def __str__(self):
