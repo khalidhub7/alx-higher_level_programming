41ac0bcae9cb2210e41a77074cfd6deb63436ade
khalidhub7 <louguikhalid7@gmail.com>
2024-01-16 08:57:26 -0800

    up

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,3 +87,23 @@ class Rectangle(Base):
         return '[Rectangle] ({}) {}/{} - {}/{}'.\
             format(self.id, self.x, self.y, self.width,
                    self.height)
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''update helper'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''update function'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
