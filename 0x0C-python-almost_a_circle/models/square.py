f5222aa0d839a7b594c100efdde570ec403a285b
khalidhub7 <louguikhalid7@gmail.com>
2024-01-17 00:06:17 -0800

    up

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' square module'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''the square class'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''constructor'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''Return string info to the user about square.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
