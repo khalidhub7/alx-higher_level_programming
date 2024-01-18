9b1f23d4881a8a7dfee9a3ea96ccc19414789d01
khalidhub7 <louguikhalid7@gmail.com>
2024-01-17 21:18:10 -0800

    up

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        '''return json string'''
+        return json.dumps(list_dictionaries)
