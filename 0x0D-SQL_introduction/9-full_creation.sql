4f1ceb51b4fed61fa5bb0260514b0d5949c51114
khalidhub7 <louguikhalid7@gmail.com>
2024-02-14 17:39:58 -0800

    up

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,5 @@
 -- script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
-USE hbtn_0c_0;
-
+-- The database name will be passed as an argument
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
