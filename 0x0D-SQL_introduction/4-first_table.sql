fa6fadf090e84362716d41ea9514d0ac99f2d9ce
khalidhub7 <louguikhalid7@gmail.com>
2024-02-14 16:38:26 -0800

    up

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- create table called 'first_table' in the current database in your MySQL server
 -- you pass database as an argument
-CREATE TABLE IF NOT EXISTS first_table (id INT name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
