83e0838d92c93dcf8a63b20a2779048a76868292
khalidhub7 <louguikhalid7@gmail.com>
2024-03-11 07:52:05 -0700

    push task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+argv.forEach((value, key) => {
+  if (key === 2) {
+    console.log(`${value}`);
+  } else {
+    console.log('No argument');
+  }
+});
