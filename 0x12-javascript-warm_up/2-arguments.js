d1f659437a88f931e5a5187b2f8cf446097c3dde
khalidhub7 <louguikhalid7@gmail.com>
2024-03-11 07:05:11 -0700

    pushing task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,12 @@
-empty for now
+#!/usr/bin/node
+const {argv} = require('node:process')
+const args = argv.length - 2
+if (args === 0) {
+  console.log('No argument')
+}
+else if (args === 1) {
+  console.log('Argument found')
+}
+else {
+  console.log('Arguments found')
+}
