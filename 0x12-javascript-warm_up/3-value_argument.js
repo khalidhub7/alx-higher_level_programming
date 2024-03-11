03ee3b8c0c455610275c3830344e980b5aba2f36
khalidhub7 <louguikhalid7@gmail.com>
2024-03-11 09:53:51 -0700

    push task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,11 +2,11 @@
 const { argv } = require('node:process');
 let number_of_args_passed = 0;
 argv.forEach((value, key) => {
+  number_of_args_passed++;
   if (key === 2) {
     console.log(`${value}`);
-    number_of_args_passed++;
   }
 });
-if (number_of_args_passed === 0) {
+if (number_of_args_passed === 2 || number_of_args_passed === 1) {
   console.log('No argument');
 }
