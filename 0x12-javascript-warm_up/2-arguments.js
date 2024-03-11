357ccfdb79073971278e621cbb660f177d53950f
khalidhub7 <louguikhalid7@gmail.com>
2024-03-11 07:29:11 -0700

    fix semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,7 @@ const { argv } = require('node:process');
 const args = argv.length - 2;
 if (args === 0) {
   console.log('No argument');
-}
-else if (args === 1) {
+} else if (args === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
