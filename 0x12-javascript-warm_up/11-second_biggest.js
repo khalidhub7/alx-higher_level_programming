ae9cc12609bb316431a72a51457bb031ce8a3841
khalidhub7 <louguikhalid7@gmail.com>
2024-03-12 07:24:20 -0700

    fix output

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
+const { argv } = require('node:process');
 const mylist = [];
 
 /* extract elements in command line */
 argv.forEach((value, key) => {
-  if (key >= 2) { // Start from index 2 to skip the first two arguments
+  /* Start from index 2 to skip the first two arguments */
+  if (key >= 2) {
     mylist.push(Number(value));
   }
 });
 
-if (mylist.length < 2) { // Check if there are less than two numbers
+if (mylist.length < 2) {
+  /* Check if there are less than two numbers */
   console.log('0');
 } else {
   /* Sort the list in descending order */
-  mylist.sort();
-  mylist.reverse();
+  const sortedList = mylist.sort((a, b) => b - a);
   /* Print the second largest number */
-  console.log(mylist[1]);
+  console.log(sortedList[1]);
 }
