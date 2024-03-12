4f1c389a5718d43cb34c1bea280acadaadc25bbb
khalidhub7 <louguikhalid7@gmail.com>
2024-03-12 06:00:59 -0700

    pushing tasks 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+const list = [];
+let nums = 0;
+
+/* extract elements in command line */
+argv.forEach((value, key) => {
+  nums = value;
+  list.push(Number(nums));
+});
+
+if (argv.length < 4) {
+  console.log('0');
+} else {
+  const NewList = [];
+  let i = 2;
+  /* removing index 0 && index 1 in list  */
+  while (i < argv.length) {
+    NewList.push(list[i]);
+    i++;
+  }
+  NewList.sort();
+  NewList.reverse();
+  console.log(NewList[1]);
+}
