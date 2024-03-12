3ea13284b6830f30d98653a03b44de42cdefc163
khalidhub7 <louguikhalid7@gmail.com>
2024-03-12 06:46:34 -0700

    fix output

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
 const mylist = [];
 let nums = 0;
 
 /* extract elements in command line */
-process.argv.forEach((value, key) => {
-  if (key >= 2) { // Start from index 2 to skip the first two elements (node executable and script file)
-    nums = value;
-    mylist.push(Number(nums));
-  }
+argv.forEach((value, key) => {
+  nums = value;
+  mylist.push(Number(nums));
 });
 
-if (process.argv.length < 4) {
+if (argv.length < 4) {
   console.log('0');
 } else {
   const NewList = [];
   let i = 2;
   /* removing index 0 && index 1 in list  */
-  while (i < process.argv.length) {
+  while (i < argv.length) {
     NewList.push(mylist[i - 2]); // Adjusting index to match mylist
     i++;
   }
