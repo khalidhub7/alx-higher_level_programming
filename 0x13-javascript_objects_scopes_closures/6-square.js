ebe363f364b5f7509f5931dc26da5d20cace8d46
khalidhub7 <louguikhalid7@gmail.com>
2024-03-12 22:21:12 -0700

    pushing task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let i = 0;
+    if (!c) {
+      while (i < this.height) {
+        console.log('X'.repeat(this.width));
+        i++;
+      }
+    } else if (c) {
+      while (i < this.height) {
+        console.log(c.repeat(this.width));
+        i++;
+      }
+    }
+  }
+}
+module.exports = Square;
