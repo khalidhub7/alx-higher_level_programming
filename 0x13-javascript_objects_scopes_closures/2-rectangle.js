ba5c2daa2211e28317ad4e09ea2ded04ef483d17
khalidhub7 <louguikhalid7@gmail.com>
2024-03-12 18:21:45 -0700

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,9 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (w && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      this.width;
-      this.height;
     }
   }
 }
