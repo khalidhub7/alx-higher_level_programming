027513987ba5c8ae39b66f645f1a1096a4ea3831
khalidhub7 <louguikhalid7@gmail.com>
2024-03-16 14:54:21 -0700

    task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""Get all states and display them."""
+'''
+get and print all states
+'''
 
+import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-
-    db = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
-    cur = db.cursor()
-
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
-    all_rows = cur.fetchall()
-
-    for one in all_rows:
-        print(one)
-
-    cur.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], database=argv[3])
+    curr = db.cursor()
+    myquery = "SELECT id, name FROM states ORDER BY id ASC;"
+    curr.execute(myquery)
+    results = curr.fetchall()
+    for i in results:
+        print(i)
+    curr.close()
     db.close()
