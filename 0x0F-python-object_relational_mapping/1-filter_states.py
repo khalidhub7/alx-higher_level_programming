5ead7889ea1c58c7608393da52c001531378dc92
khalidhub7 <louguikhalid7@gmail.com>
2024-03-16 16:33:22 -0700

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,15 +4,15 @@ get and print all states
 with simple filter
 '''
 
+
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     datab = MySQLdb.connect(user=argv[1], passwd=argv[2], database=argv[3])
     curr = datab.cursor()
-    start_with = "N"
-    myquery = "SELECT id, name FROM states WHERE name \
-               LIKE '{}%' ORDER BY id ASC;".format(start_with)
+    myquery = "SELECT id, name FROM states WHERE name\
+         LIKE 'N%' ORDER BY id ASC;"
     curr.execute(myquery)
     results = curr.fetchall()
     for i in results:
