dd7176bf9f45ed564ee2481196fcc7f80430a669
khalidhub7 <louguikhalid7@gmail.com>
2024-03-16 16:10:47 -0700

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+get and print all states
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    datab = MySQLdb.connect(user=argv[1], passwd=argv[2], database=argv[3])
+    curr = datab.cursor()
+    start_with = "N"
+    myquery = "SELECT id, name FROM states WHERE name LIKE '{}' ORDER BY id ASC;"\
+        .format(start_with)
+    curr.execute(myquery)
+    results = curr.fetchall()
+    for i in results:
+        print(i)
+    curr.close()
+    datab.close()
