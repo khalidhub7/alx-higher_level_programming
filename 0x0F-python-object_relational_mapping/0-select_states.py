0c681cb2a7a323bbcb414b138f8729489f707125
khalidhub7 <louguikhalid7@gmail.com>
2024-03-16 15:33:55 -0700

    task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,10 @@ get and print all states
 '''
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], database=sys.argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], database=argv[3])
     curr = db.cursor()
     myquery = "SELECT id, name FROM states ORDER BY id ASC;"
     curr.execute(myquery)
