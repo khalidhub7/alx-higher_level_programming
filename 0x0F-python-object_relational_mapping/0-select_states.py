ca81fd1d90c2b115b76f01888061263f5ee6c6bb
khalidhub7 <louguikhalid7@gmail.com>
2024-03-16 14:20:13 -0700

    task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,13 @@
 
 import MySQLdb
 
-db = MySQLdb.connect(username='', passwd='', database='')
-curr = MySQLdb.cursor()
-query = "SELECT * FROM hbtn_0e_0_usa"
-curr.execute(query)
-resu = curr.fettchall()
-for i in resu:
-    print(i)
-db.close()
-curr.close()
+if __name__=='__main__':
+    db = MySQLdb.connect(host="localhost", user='', passwd='', database='')
+    curr = MySQLdb.cursor()
+    query = "SELECT * FROM hbtn_0e_0_usa"
+    curr.execute(query)
+    resu = curr.fettchall()
+    for i in resu:
+        print(i)
+    db.close()
+    curr.close()
