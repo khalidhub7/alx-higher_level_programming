89c6b884b79b363aed1d972aa19842eb7c92b4ae
khalidhub7 <louguikhalid7@gmail.com>
2024-03-17 08:46:00 -0700

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  lists all cities from the database  """
+
+import MySQLdb
+from sys import argv
+if __name__=='__main__':
+    datab = MySQLdb.connect(
+        user=argv[1], passwd=argv[2], database=argv[3]
+    )
+    curs = datab.cursor()
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    curs.execute(query)
+    results = curs.fetchall()
+    for i in results:
+        print(i)
