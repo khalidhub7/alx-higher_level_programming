8a02ce50a30d744c3bc59c31fbbfa924a5077e24
khalidhub7 <louguikhalid7@gmail.com>
2024-03-16 17:16:01 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         user=argv[1], passwd=argv[2], database=argv[3]
     )
     curr = datab.cursor()
-    myquery = "SELECT id, name FROM states ORDER BY id ASC;"
+    myquery = "SELECT * FROM states ORDER BY id ASC;"
     curr.execute(myquery)
     results = curr.fetchall()
     for i in results:
