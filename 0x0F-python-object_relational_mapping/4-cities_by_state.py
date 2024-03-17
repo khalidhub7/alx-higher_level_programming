954c5988f78d97bf53cfabb2d895b2ae75c048c6
khalidhub7 <louguikhalid7@gmail.com>
2024-03-17 08:53:21 -0700

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,30 +1,16 @@
 #!/usr/bin/python3
-"""Lists all cities from the database"""
+"""  lists all cities from the database  """
 
-import sys
 import MySQLdb
-
-if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print("Usage: python script.py username password database_name")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    try:
-        datab = MySQLdb.connect(user=username, passwd=password, database=database_name)
-        curs = datab.cursor()
-        query = "SELECT * FROM cities ORDER BY id ASC"
-        curs.execute(query)
-        results = curs.fetchall()
-        for row in results:
-            print(row)
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-    finally:
-        if curs:
-            curs.close()
-        if datab:
-            datab.close()
+from sys import argv
+if __name__=='__main__':
+    datab = MySQLdb.connect(
+        user=argv[1], passwd=argv[2], database=argv[3]
+    )
+    curs = datab.cursor()
+    query =  "SELECT cities.id, cities.name, states.name FROM cities JOIN\
+    states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    curs.execute(query)
+    results = curs.fetchall()
+    for i in results:
+        print(i)
