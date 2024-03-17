bd200a80ea2e4324d16287492edd375f00828fd2
khalidhub7 <louguikhalid7@gmail.com>
2024-03-16 18:32:26 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """  Get && print table with filter passed as arg. """
-
 import MySQLdb
 from sys import argv
-
-
-def process():
+if __name__=='__main__':
     datab = MySQLdb.connect(user=argv[1], passwd=argv[2], database=argv[3])
     curs = datab.cursor()
     query = 'SELECT * FROM states ORDER BY id {};'.format('ASC')
@@ -14,7 +11,3 @@ def process():
     for i in results:
         if i[1] == argv[4]:
             print(i)
-
-
-if __name__=='__main__':
-    process()
