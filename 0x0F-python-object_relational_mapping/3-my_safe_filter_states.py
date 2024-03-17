282da7f741a8f2507cdef42c53ab84689cf3fd50
khalidhub7 <louguikhalid7@gmail.com>
2024-03-16 19:40:35 -0700

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,13 +2,26 @@
 ''' same as last code, but safe from MySQL injections! '''
 import MySQLdb
 from sys import argv
+
 if __name__ == '__main__':
-    datab = MySQLdb.connect(user=argv[1], passwd=argv[2],
-    database=argv[3], state_searched=argv[4])
+    # Check if all required arguments are provided
+    if len(argv) < 5:
+        print("Usage: python script.py <username> <password> <database> <state_searched>")
+        exit(1)
+    
+    # Establish a connection to the database
+    datab = MySQLdb.connect(user=argv[1], passwd=argv[2], database=argv[3])
     curr = datab.cursor()
-    myquery = 'SELECT id, name FROM states\
-    WHERE name = %s ORDER BY id {};'.format('ASC')
+
+    # Use placeholders to avoid SQL injection
+    myquery = 'SELECT id, name FROM states WHERE name = %s ORDER BY id ASC;'
     curr.execute(myquery, (argv[4],))
+    
+    # Fetch and print results
     results = curr.fetchall()
-    for i in results:
-        print(i)
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    curr.close()
+    datab.close()
