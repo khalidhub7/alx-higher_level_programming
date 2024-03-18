bed3373931652a30e72d488ea8f518b3caecd1c2
khalidhub7 <louguikhalid7@gmail.com>
2024-03-18 14:43:59 -0700

    first practice with sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,13 +5,13 @@ from sqlalchemy import Column
 from sqlalchemy import Integer, String
 
 
-if __name__ == '__main__':
-    Base = declarative_base()
-    class State(Base):
-        """ define class
-        State inherite from Base """
-        __tablename__ = "states"
-        id = Column(Integer, primary_key=True, 
-                    autoinrement=True, nullable=False)
 
-        name = Column(String(128), nullable=False)
+Base = declarative_base()
+class State(Base):
+    """ define class
+    State inherite from Base """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, 
+                autoinrement=True, nullable=False)
+
+    name = Column(String(128), nullable=False)
