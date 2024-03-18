3459f6d0233f4fdd9b09ab28c20fb2834106f337
khalidhub7 <louguikhalid7@gmail.com>
2024-03-18 14:48:44 -0700

    first practice with sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ sqlalchemy practice """
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
 
 class State(Base):
     """ Define class State inheriting from Base """
