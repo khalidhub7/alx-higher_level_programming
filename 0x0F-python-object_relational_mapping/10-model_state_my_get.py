adb959072f1e69d16ff121dc72f8cf60a6151014
khalidhub7 <louguikhalid7@gmail.com>
2024-03-18 19:22:44 -0700

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 '.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(datab)
     session = sessionmaker(bind=datab)()
-    results = session.query(State).filter(State.name == argv[4]).first()
+    results = session.query(State).filter(State.name == "{}\
+".format(argv[4])).order_by(State.id.asc()).all()
     if results == []:
         print('Not found')
     else:
