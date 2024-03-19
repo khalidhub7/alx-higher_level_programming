c22faef7a78f45b6a130e0297caadf7d7e8cde03
khalidhub7 <louguikhalid7@gmail.com>
2024-03-18 19:11:09 -0700

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""link class to database_table with some filter
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    datab = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
+'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(datab)
+    session = sessionmaker(bind=datab)()
+    results = session.query(State).filter(State.name == argv[4]).first()
+    if results:
+        print(state.id)
+    else:
+        print('Not found')
