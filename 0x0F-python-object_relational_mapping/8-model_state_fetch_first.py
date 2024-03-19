168d4249514a6012d39a131b7b67d8420d6f907f
khalidhub7 <louguikhalid7@gmail.com>
2024-03-18 17:24:46 -0700

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,10 +8,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
+    datab = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
 '.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
+    Base.metadata.create_all(datab)
+    session = sessionmaker(bind=datab)()
     first = session.query(State).filter_by(id=1).first()
     try:
         print(first.id, first.name, sep=': ')
