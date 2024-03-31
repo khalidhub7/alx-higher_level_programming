5ed19cceb309ebdb058579b2e5cb363a2749fb5f
khalidhub7 <louguikhalid7@gmail.com>
2024-03-30 23:09:32 -0700

    task 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,2 @@
---  lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
+--  lists all records of table
+SELECT score, name FROM second_table ORDER BY score DESC;
