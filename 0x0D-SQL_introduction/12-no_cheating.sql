619a139c1957f0e3580cdccbf1248ef17fb31868
khalidhub7 <louguikhalid7@gmail.com>
2024-03-30 23:32:32 -0700

    task 12

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,2 @@
--- script that updates the score of Bob to 10 in the table second_table
-UPDATE second_table
-SET score = 10
-WHERE name = 'bob';
+-- update  score of Bob
+UPDATE second_table SET score = 10 WHERE name = 'bob';
