584d2666648fd54dc4f15457586174e5ef4ecd1d
khalidhub7 <louguikhalid7@gmail.com>
2024-03-31 23:40:15 -0700

    task3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- create a table force_name
-CREATE TABLE IF NOT EXISTS  force_name (id INT, name VARCHAR(256) NULLABLE=OFF)
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NULLABLE=OFF);
