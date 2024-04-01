f45bee4ee26e8846bf9abf2d83e525ef61e8f2eb
khalidhub7 <louguikhalid7@gmail.com>
2024-03-31 21:18:46 -0700

    adv 1

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- converts hbtn_0c_0 database to UTF8
-USE hbtn_0c_0; ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256)
+USE hbtn_0c_0;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
