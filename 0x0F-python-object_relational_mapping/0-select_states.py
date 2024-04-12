107be191ec197f04f6312c085132e0ec31147fb0
khalidhub7 <louguikhalid7@gmail.com>
2024-04-12 06:17:53 -0700

    fix  PEP 8 style guidelines

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
-""" lists all states using py """
+"""Lists all states using PyMySQL."""
 
 if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
     conx = MySQLdb.connect(
-        host='localhost', port=3306, 
+        host='localhost', port=3306,
         user=argv[1], passwd=argv[2], database=argv[3]
-        )
+    )
 
-    _cursor = conx.cursor()
+    cursor = conx.cursor()
     query = "SELECT * FROM states"
 
-    _cursor.execute(query)
-    result = _cursor.fetchall()
+    cursor.execute(query)
+    result = cursor.fetchall()
 
     for i in result:
         print(i)
 
-    _cursor.close()
-    _data.close()
+    cursor.close()
+    conx.close()
