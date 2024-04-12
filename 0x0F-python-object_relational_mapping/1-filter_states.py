4dd95de7ea49ff0b351337c4cd7f28395e5287f9
khalidhub7 <louguikhalid7@gmail.com>
2024-04-12 06:31:58 -0700

    fix  PEP 8 style guidelines

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-""" lists all states with some filter """
+"""Lists all states with some filter."""
 
 if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
     conx = MySQLdb.connect(
-        host='localhost', port=3306, 
+        host='localhost', port=3306,
         user=argv[1], passwd=argv[2], db=argv[3]
-        )
+    )
+
     query = 'SELECT * FROM states'
     _curs = conx.cursor()
 
@@ -19,6 +20,6 @@ if __name__ == '__main__':
     for i in result:
         if str(i[1]).startswith('N'):
             print(i)
-    
-    _cursor.close()
+
+    _curs.close()
     conx.close()
