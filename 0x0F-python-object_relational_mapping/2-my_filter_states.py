23e8bef96c167a3b78417f6033f5bd4b13153e1d
khalidhub7 <louguikhalid7@gmail.com>
2024-04-12 06:35:42 -0700

    fix  PEP 8 style guidelines

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lists all states + arg to find"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+
+    _data = MySQLdb.connect(
+        host='localhost', port=3306,
+        user=argv[1], passwd=argv[2], database=argv[3]
+    )
+    _curs = _data.cursor()
+    _query = 'SELECT * FROM states WHERE name LIKE "{}"'.format(argv[4])
+
+    _curs.execute(_query)
+    result = _curs.fetchall()
+
+    for i in result:
+        print(i)
+    
+    _curs.close()
+    _data.close()
