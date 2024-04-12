58b8a307419dd1d89633eefe1f5b40403da81fc3
khalidhub7 <louguikhalid7@gmail.com>
2024-04-12 07:03:21 -0700

    fix  PEP 8 style guidelines

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
 
     conx = MySQLdb.connect(
         host='localhost', port=3306,
-        user=argv[1], passwd=argv[2], db=argv[3]
+        user=argv[1], passwd=argv[2], database=argv[3]
     )
     curs = conx.cursor()
     query = 'SELECT * FROM states WHERE name LIKE "{}"'\
-        .format(argv[4])
+        .format(str(argv[4]))
 
     curs.execute(query)
     result = curs.fetchall()
