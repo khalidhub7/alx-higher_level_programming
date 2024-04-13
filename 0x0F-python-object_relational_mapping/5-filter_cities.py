49b2073b9b1755e5ee05624847394d2d1f8bcc7d
khalidhub7 <louguikhalid7@gmail.com>
2024-04-13 04:35:43 -0700

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 
-"""Lists all cities.
-with some filter"""
+"""Lists all cities with some filter."""
 
 if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
     conx = MySQLdb.connect(
-        host="localhost", port=3306, 
+        host="localhost", port=3306,
         user=argv[1], passwd=argv[2], db=argv[3]
     )
 
     curs = conx.cursor()
     query = 'SELECT cities.id, cities.name, states.name ' \
-        'FROM cities INNER JOIN states ' \
-        'ON states.id = cities.state_id'
+            'FROM cities INNER JOIN states ' \
+            'ON states.id = cities.state_id'
 
     curs.execute(query)
     results = curs.fetchall()
