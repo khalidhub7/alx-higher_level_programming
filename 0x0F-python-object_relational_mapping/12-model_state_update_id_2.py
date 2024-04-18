28c50ffda094fe15381a6bcca941ebf90142cea8
khalidhub7 <louguikhalid7@gmail.com>
2024-04-18 03:32:57 -0700

    update obj

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-""" Get a state
-"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import (create_engine)
+"""sql_alchemy"""
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
-'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = 'New Mexico'
-    session.commit()
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+    from sys import argv
+
+    engine = create_engine(
+        'mysqldb://{}: {}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
+    )
+    Base.metadata.create_all(bind=engine)
+    sess = Session(bind=engine)
+    obj = State(id=2, name='New Mexico')
+    sess.merge(obj)
+    sess.commit()
+
+    result = sess.query(State).all()
+    for i in result:
+        print('{}: {}'.format(i.id, i.name))
