d0fab00e9ae9233ad9fabe0430299735dcec478d
khalidhub7 <louguikhalid7@gmail.com>
2024-04-17 17:43:01 -0700

    link class with db && fix pep8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,9 +9,8 @@ if __name__ == '__main__':
     from sqlalchemy.orm import Session
 
     engine = create_engine(
-        'mysql://{}:{}@localhost:3306/{}'\
-            .format(argv[1], argv[2], argv[3])
-        )
+        'mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
+    )
     Base.metadata.create_all(bind=engine)
     sess = Session(bind=engine)
     result = sess.query(State).order_by(State.id).all()
