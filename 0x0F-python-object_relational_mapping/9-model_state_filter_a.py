7c1dee94bf7f72a602ade034e51bb471fa1fc8e0
khalidhub7 <louguikhalid7@gmail.com>
2024-04-17 18:26:00 -0700

    filtering alchemy

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import (create_engine)
+"""sql_alchemy"""
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
-'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
-    all_a = session.query(State).filter(State.name.like('%a%\
-')).order_by(State.id.asc()).all()
-    for first in all_a:
-        print(first.id, first.name, sep=': ')
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy.orm import Session
+    from sqlalchemy import create_engine
+    from sys import argv
+
+    engine = create_engine(
+        "mysql://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3])
+    )
+    Base.metadata.create_all(bind=engine)
+    sess = Session(bind=engine)
+    result = sess.query(State).filter(State.name.like('%a%')).all()
+
+    for i in result:
+        print('{}: {}'.format(i.id, i.name))
