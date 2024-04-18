a2f86ba944d5e0fea866197c8c978757501819bd
khalidhub7 <louguikhalid7@gmail.com>
2024-04-17 18:11:19 -0700

    filtering alchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     engine = create_engine(
         'mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
-        )
+    )
     Base.metadata.create_all(bind=engine)
     sess = Session(bind=engine)
     results = sess.query(State).order_by(State.id).first()
