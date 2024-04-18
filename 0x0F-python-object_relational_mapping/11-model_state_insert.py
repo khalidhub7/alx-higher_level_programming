b1b294c76f6719e1bed9f6326e3e315158b8ee19
khalidhub7 <louguikhalid7@gmail.com>
2024-04-18 02:41:42 -0700

    add new obj

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-""" Get a state
-"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import (create_engine)
+"""sql_alchemy"""
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
-'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+    from sys import argv
+
+    engine = create_engine(
+        'mysql://{}:{}@localhost:3306/{}'\
+            .format(argv[1], argv[2], argv[3])
+    )
+    Base.metadata.create_all(bind=engine)
     new_obj = State(name='Louisiana')
-    session.add(new_obj)
-    session.commit()
-    print(new_obj.id)
+
+    sess = Session(bind=engine)
+    sess.add(new_obj)
+    sess.commit()
+
+    # to display all
+    """ new = sess.query(State).all()
+    for i in new:
+        print('{}: {}'.format(i.id, i.name)) """
+    
+    new = sess.query(State).filter(State.name == 'Louisiana').all()
+    print("{}".format(new[0].id))
