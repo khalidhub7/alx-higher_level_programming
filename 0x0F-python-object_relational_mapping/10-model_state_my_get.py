e990db4def3c566f8603b87e63720b092f10cd58
khalidhub7 <louguikhalid7@gmail.com>
2024-04-17 18:52:52 -0700

    filtering alchemy

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     sess = Session(bind=engine)
     result = sess.query(State).filter(State.name.like("{}".format(argv[4]))).all()
 
-    if result != []:
-        print('{}'.format(result[0].id))
     if result == []:
         print('Not found')
+    else:
+        print(result[0].id)
