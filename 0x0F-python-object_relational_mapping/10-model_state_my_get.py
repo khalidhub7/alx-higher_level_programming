c3050baf6a649b33ac8836de71f641bee9cc37d1
khalidhub7 <louguikhalid7@gmail.com>
2024-04-17 18:47:31 -0700

    filtering alchemy

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
     )
     Base.metadata.create_all(bind=engine)
     sess = Session(bind=engine)
-    result = sess.query(State).filter(State.name.like("{}".format(argv[4]))).all()
+    result = sess.query(State).filter(State.name.like("{}"\
+        .format(argv[4]))).all()
 
     if result != []:
         print('{}'.format(result[0].id))
