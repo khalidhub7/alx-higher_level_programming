3af1f77cdcb4f77a08c5dcaad137f968739fd2f1
khalidhub7 <louguikhalid7@gmail.com>
2024-04-28 08:10:03 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send GET request to URL and display body of response
+# display the body of the response only if status code is 200
 [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -sL "$1"
