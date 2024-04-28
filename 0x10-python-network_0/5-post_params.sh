16f6aa20c0d4314cc12d8d36f0d997ddc93e4302
khalidhub7 <louguikhalid7@gmail.com>
2024-04-28 10:49:34 -0700

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  sends POST request, and displays the body of the response
-curl -sX POST -d "email:test@gmail.com&subject:I will always be here for PLD" "$1"
+curl -s -X POST -d "email:test@gmail.com&subject:I will always be here for PLD" "$1"
