bc52737a4c2be3ce85c1ecca98f7cf5274ff4815
khalidhub7 <louguikhalid7@gmail.com>
2024-04-28 09:33:33 -0700

    display value of a header variable

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends GET request to diplay value of some variable
-curl -sI "$1" | grep "X-School-User-Id" | cut -d ' ' -f2
+curl -sI "$1" | grep "X-School-User-Id" | cut -d ':' -f2 | tr -d '[:space:]'
