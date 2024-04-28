854319573fcd51bb1cbf7d7682f5e1afde3b1242
khalidhub7 <louguikhalid7@gmail.com>
2024-04-28 12:46:41 -0700

    peak num

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-""" Script to find a peak in a list of unsorted integers """
 
+"""Find a peak in a list."""
 
 def find_peak(list_of_integers):
-    """ Finds a peak in a list of unsorted integers """
-    if not list_of_integers:
+    if len(list_of_integers) == 0:
         return None
 
-    def binary_search(arr, low, high):
-        """ Helper function to perform binary search for a peak element """
-        if low == high:
-            return arr[low]
+    peak = list_of_integers[0]
+    for i in list_of_integers:
+        if i > peak:
+            peak = i
 
-        mid = (low + high) // 2
-        if ((mid == 0 or arr[mid - 1] <= arr[mid]) and
-                (mid == len(arr) - 1 or arr[mid + 1] <= arr[mid])):
-            return arr[mid]
-        elif mid > 0 and arr[mid - 1] > arr[mid]:
-            return binary_search(arr, low, mid - 1)
-        else:
-            return binary_search(arr, mid + 1, high)
-
-    return binary_search(list_of_integers, 0, len(list_of_integers) - 1)
+    return peak
