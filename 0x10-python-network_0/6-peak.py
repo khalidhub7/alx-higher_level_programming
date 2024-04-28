ba11e2d375e931c65baf1e52ba527a7be5609728
khalidhub7 <louguikhalid7@gmail.com>
2024-04-28 12:08:56 -0700

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,6 +8,9 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
     elif len (list_of_integers) != 0:
-        list_of_integers.sort()
-        list_of_integers.reverse()
-        return (list_of_integers[0])
+        peak = list_of_integers[0]
+        for i in list_of_integers:
+            if i:
+                if i > peak:
+                    peak = i
+        return peak
