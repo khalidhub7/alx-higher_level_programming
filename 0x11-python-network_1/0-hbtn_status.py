7639238d49de6dc9be4ae52af3cf25300841a50e
khalidhub7 <louguikhalid7@gmail.com>
2024-04-29 08:55:27 -0700

    fetches url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as opened_url:
-    responce = opened_url.read()
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as opened_url:
+        responce = opened_url.read()
 
-for i in responce:
-    print(i)
+    for i in responce:
+        print(i)
