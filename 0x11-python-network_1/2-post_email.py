5aa6db4ba8ec43a6ffc3663593746e512257c4d1
khalidhub7 <louguikhalid7@gmail.com>
2024-04-29 12:21:32 -0700

    post method

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,11 +10,14 @@ if __name__ == '__main__':
     email_v = argv[2]
     values = {'email': email_v}
 
+     # Encode the dictionary into a URL-encoded string
     data = urllib.parse.urlencode(values)
+
     data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
-
+    # Convert the URL-encoded string to bytes using UTF-8 encoding
     with urllib.request.urlopen(req) as page:
+        # Read the response body as bytes and decode it into a UTF-8 string
         var = page.read().decode('utf-8')
 
     print(var)
