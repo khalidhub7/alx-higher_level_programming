779a8eceb2ad82352c1dd1ab44dc5d7ac9d70b06
khalidhub7 <louguikhalid7@gmail.com>
2024-04-29 09:33:54 -0700

    fetches url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" that fetches """
-
+'''Fetches a URL'''
 
 import urllib.request
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as responce:
-        page = responce.read()
-    print('Body response:\n\
-    - type: {}\n\
-    - content: {}\n\
-    - utf8 content: {}'\
-    .format(type(page), page, page.decode('utf8')))
+    with urllib.request.urlopen(url) as response:
+        page = response.read()
+    print('Body response:\n'
+          f'\t- type: {type(page)}\n'
+          f'\t- content: {page}\n'
+          f'\t- utf8 content: {page.decode("utf8")}')
