2ba2ad55b4648c89fa3d97d164fe17de2b5528ca
khalidhub7 <louguikhalid7@gmail.com>
2024-05-01 16:41:19 -0700

    handle code status

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     url = argv[1]
     req = requests.get(url)
     if req.code_status >= 400:
-        print(f'Error code: {code_status}')
+        print(f'Error code: {req.code_status}')
     else:
         print(req.text)
