628b63a5d961f6812a560fe83de0049ba20e249e
khalidhub7 <louguikhalid7@gmail.com>
2024-05-01 14:31:28 -0700

    handle error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ handle error """
 
-import urllib
+import urllib.request
 from sys import argv
 from urllib.error import HTTPError
 
 if __name__ == '__main__':
     url = argv[1]
     try:
-        urllib.request.urlopen(url)
+        with urllib.request.urlopen(url) as page:
+            page = page.read().decode('utf-8')
+        print(page)
     except HTTPError as rr:
         print(f'Error code: {rr.code}')
