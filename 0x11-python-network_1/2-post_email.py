782c3cd8cf5e81c99bddfaa459a3ca5e3e9c6728
khalidhub7 <louguikhalid7@gmail.com>
2024-05-01 14:15:01 -0700

    post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as post:
-        page = post.read()
+        page = post.read().decode("utf-8")
     print(page)
