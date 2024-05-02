e303aefd4197c20a70b46be2772cc865ebfe0250
khalidhub7 <louguikhalid7@gmail.com>
2024-05-01 17:12:12 -0700

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""post a letter, get id and name(name that match that letter)"""
+""" handle status code
+ using requests package"""
 
-
+import requests
 from sys import argv
-from requests import post
 
-if __name__ == "__main__":
-    try:
-        q = argv[1]
-    except IndexError:
-        q = ""
-
-    url = "http://0.0.0.0:5000/search_user"
-    data = {'q': q}
-    req = post(url, data=data)
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    if argv[1]:
+        data = {'q': argv[1]}
+    else:
+        data = {'q': ''}
+    req = requests.post(url, data)
 
     if req.headers.get('content-type') == 'application/json':
-        content = req.json()
-        if content:
-            the_id = content.get('id')
-            name = content.get('name')
-            print("[{}] {}".format(the_id, name))
+        if req.json():
+            print(f'[{req.json().get("id")}] {req.json().get('name')}')
         else:
-            print("No result")
+            print('No result')
     else:
-        print("Not a valid JSON")
+        print('Not a valid JSON')
