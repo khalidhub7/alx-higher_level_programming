e6eb4af011f26ddc4e047ddda43940aa8cd74762
khalidhub7 <louguikhalid7@gmail.com>
2024-08-10 15:19:46 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const request = require('request');
-
+const tool = require('request');
 const url = process.argv[2];
 
-request.get(url, (err, resp) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('code:', resp.statusCode);
-  }
-});
+function showstatus (url) {
+  tool(url, (err, response) => {
+    if (!err) {
+      console.log('code: ' + response.statusCode);
+    }
+  });
+}
+showstatus(url);
