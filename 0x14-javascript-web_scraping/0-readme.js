c43d67da86ed30d61f51115e1397e2f431e6d604
khalidhub7 <louguikhalid7@gmail.com>
2024-08-10 13:14:39 -0700

    reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const file_tool = require('fs');
-const file_path = process.argv[2];
+const filetool = require('fs');
+const filepath = process.argv[2];
 
-function read_file (file_path) {
-  file_tool.readFile(file_path, 'utf-8', (err, data) => {
+function readfile (filepath) {
+  filetool.readFile(filepath, 'utf-8', (err, data) => {
     if (err) {
       console.log(err);
       return 1;
@@ -14,5 +14,4 @@ function read_file (file_path) {
     }
   });
 }
-
-read_file(file_path);
+readfile(filepath);
