d21973d071005d8ac26f0239a2595f9170ed089d
khalidhub7 <louguikhalid7@gmail.com>
2024-08-10 17:33:08 -0700

    num of movies where “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,26 +3,23 @@ const tool = require('request');
 const url = process.argv[2];
 const caracter = 'https://swapi-api.alx-tools.com/api/people/18/';
 
-function countfilms (url) {
+function countfilms(url) {
   tool(url, (err, response, body) => {
     if (err) {
-      console.error(err);
+      return console.error(err);
     }
+
     const data = JSON.parse(body).results;
-    let i = 0;
-    let j = 0;
     let count = 0;
-    while (i < data.length) {
-      while (j < data[i].characters.length) {
-        if (data[i].characters[j] === caracter) {
-          count += 1;
-        }
-        j++;
+
+    data.forEach(film => {
+      if (film.characters.includes(caracter)) {
+        count++;
       }
-      i++;
-      j = 0;
-    }
+    });
+
     console.log(count);
   });
 }
+
 countfilms(url);
